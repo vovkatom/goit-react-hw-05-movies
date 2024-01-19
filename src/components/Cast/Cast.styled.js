d814dd5d49aa36e97cb6ff8329cd0c68be78e8d7
vovkatom <36505066+vovkatom@users.noreply.github.com>
2024-01-20 00:50:10 +0200

    fix1

--- src/components/Cast/Cast.styled.js
+++ src/components/Cast/Cast.styled.js
@@ -4,6 +4,12 @@ export const List = styled.ul`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
   margin-bottom: 20px;
+
+  @media screen and (max-width: 768px) {
+    grid-template-columns: repeat(1, 1fr);
+    margin: auto;
+    
+  }
 `;
 
 export const Item = styled.li`
